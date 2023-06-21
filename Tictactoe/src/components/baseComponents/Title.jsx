087a9852388b44503d87ba5ba07ba1097ba72f22
Paul Llanque <llanquepol@gmail.com>
2023-06-21 18:00:36 -0500

    add title component

---
+++ Tictactoe/src/components/baseComponents/Title.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <h1>
+      <span className="title text-red-400">Tic</span>
+      <span className="title text-white">-</span>
+      <span className="title text-yellow-400">Tac</span>
+      <span className="title text-white">-</span>
+      <span className="title text-green-400">Toe</span>
+    </h1>
+  );
+};
+
+export default Title;
